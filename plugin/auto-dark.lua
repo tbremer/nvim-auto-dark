8663e23d5a9dc5c3b19cf248fc2ab3f5ab0bfb6f
Tom Bremer <tom@tbremer.com>
2024-03-02 23:49:25 -0600

    Update auto-dark.lua

--- plugin/auto-dark.lua
+++ plugin/auto-dark.lua
@@ -2,7 +2,6 @@ local min_version = 'nvim-0.10.0'
 
 if vim.fn.has(min_version) == 0 then
   vim.api.nvim_err_writeln("tbremer/nvim-auto-dark requires at least " .. min_version)
-  print('not supported')
   return;
 end
 
